a513085e8e50661e8c195cad8811db09e7bb23b9
Nischal Subedi <nischal1337@gmail.com>
2024-07-29 09:06:17 +0545

    tailwind setup

--- src/components/RevenueCards.jsx
+++ src/components/RevenueCards.jsx
@@ -1,3 +1,9 @@
-export const RevenueCard = () => {
+export const RevenueCard = ({ title,
+    showWarning,
+    orderCount,
+    amount }) => {
+    return <div className="bg-whitek rounded shadow-sm">
+
+    </div>
 
 }
